3520e238898917ed868fc2df379aaa24689ae261
tayoolaigbe <tayoolaigbe@outlook.com>
2024-11-29 15:17:02 +0000

    completed the landing page CallToAction

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,11 @@
+import CallToAction from "./_components/call-to-action"
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <CallToAction />
+            </div>
+        </main>
     )
 }
