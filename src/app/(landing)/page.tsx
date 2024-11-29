67b1c48b207b256840138002d0c8a716b39fefe7
tayoolaigbe <tayoolaigbe@outlook.com>
2024-11-29 15:22:09 +0000

    completed the dashboard-snippet

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,10 +1,12 @@
 import CallToAction from "./_components/call-to-action"
+import DashboardSnippet from "./_components/dashboard-snippet"
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
                 <CallToAction />
+                <DashboardSnippet />
             </div>
         </main>
     )
